22d0db25a1d35eba204ca147e01ba33e8215a541
Matt Bajorek <bajorek.matthew@gmail.com>
2016-07-15 14:29:48 -0400

    Initial files

---
+++ components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        This is definitely a React app now!!
+        <TextDisplay />
+      </div>
+    )	
+  }
+
+}
+
+export default App
