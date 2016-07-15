509fff3947ab0d9a8a54d35d4a76cfaf4ac062bc
Matt Bajorek <bajorek.matthew@gmail.com>
2016-07-15 16:24:03 -0400

    Added url-loader and finished background

--- components/App.js
+++ components/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import TextDisplay from './TextDisplay';
-import './reset.css';
-import './App.css';
+import Table from './Table';
+import '../scss/reset.css';
+import '../scss/App.scss';
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        This is definitely a React app now!!
-        <TextDisplay />
-      </div>
+      <Table />
     )	
   }
 
---
+++ components/Table.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../scss/Table.scss'
+
+class Table extends Component {
+
+	render() {
+		return (
+			<div className="wrapper">
+				<div className="green-table"></div>
+				<div className="wood-trim"></div>
+			</div>
+		)
+	}
+	
+}
+
+export default Table;
